f56a09738ac5342961918432340aee45d4dbed54
qdiaps <qdiaps@gmail.com>
2024-07-29 13:20:05 +0300

    Добавил паралакс эффект заднему фону и счётчик фпс

---
+++ Assets/Scripts/UI/FPS.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+namespace UI
+{
+    public class FPS : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text _text;
+        [SerializeField] private float _updateSpeed = 1f;
+
+        private int _currentFps;
+
+        private void Start() => 
+            StartCoroutine(UpdateFPS());
+
+        private IEnumerator UpdateFPS()
+        {
+            while (true)
+            {
+                _text.text = $"{_currentFps}";
+                yield return new WaitForSeconds(_updateSpeed);
+            }
+        }
+
+        private void Update() => 
+            _currentFps = (int)(1f / Time.unscaledDeltaTime);
+    }
+}
---
+++ Assets/Scripts/UI/ParallaxEffect.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace UI
+{
+    public class ParallaxEffect : MonoBehaviour
+    {
+        [SerializeField, Range(0f, 1f)] private float _speed;
+
+        private void FixedUpdate() => 
+            transform.position += new Vector3(_speed * Time.deltaTime, 0f);
+    }
+}
